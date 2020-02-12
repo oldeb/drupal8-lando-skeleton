e351e7594e11e9812685d120e849ad0b12c37e55
Olivier Deboyser <olivier.deboyser@gmail.com>
2020-02-12 16:28:38 +0100

    Update install.sh
    Add comments

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
+set -e
 
+# Install dependencies.
 composer install -q -d /app
 
+# There are issues with the site-install drush command if this file is not copied before.
 if [[ ! -f /app/web/sites/default/settings.php ]]; then
     cp /app/web/sites/default/default.settings.php /app/web/sites/default/settings.php
 fi
 
+# Install website.
 drush si --root=/app/web --account-pass=$DRUPAL_ADMIN_PASSWORD --existing-config -y
